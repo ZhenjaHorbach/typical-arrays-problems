f77e1dba9fae1c734c02d183f90f1d12a89ae49a
ZhenjaHorbach <HorbachZhenja@yandex.by>
2020-09-28 18:16:20 +0300

    feat:result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array == undefined || array.length == 0) {
+		return 0;
+	}
+	let result = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (result > array[i]) {
+			result = array[i];
+		}
+	}
+	return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array == undefined || array.length == 0) {
+		return 0;
+	}
+	let result = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (result < array[i]) {
+			result = array[i];
+		}
+	}
+	return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array == undefined || array.length == 0) {
+		return 0;
+	}
+	let result = 0;
+	for (let i = 0; i < array.length; i++) {
+		result += array[i]
+	}
+	return result / array.length;
 }
